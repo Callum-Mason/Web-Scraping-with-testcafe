b5a2b205e698b27db1b3e75fa508446d45c6390f
Callum Mason <callum.mason@isitetv.com>
2021-05-21 09:54:36 +0100

    Fixed Race condition

--- URL.js
+++ URL.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var obj = JSON.parse(fs.readFileSync("url.json").toString('utf-8'));
 
 function GetObject() {
-    var URLTOCHECK = obj[0].URL
+    var URLTOCHECK = obj[0].URL;
     var ProductNameElement = obj[0].ProductNameElement;
     var PriceElement = obj[0].PriceElement;
     var AvalabilityElement = obj[0].AvalabilityElement;
--- website.js
+++ website.js
@@ -8,37 +8,40 @@ import {
 const fs = require("fs");
 
 
-for (let i = 0; i < 12; i++) {
+for (let i = 0; i < 3; i++) {
     let object = GetObject();
     var URL = object.URLTOCHECK
-    var ProductNameElement = object.ProductNameElement
-    var PriceElement = object.PriceElement
-    var AvalabilityElement = object.AvalabilityElement
-
     fixture `Getting Started`
         .page `${URL}`;
 
     test('My first test', async t => {
         await t
+
+        var ProductNameElement = object.ProductNameElement
+        var PriceElement = object.PriceElement
+        var AvalabilityElement = object.AvalabilityElement
         var CurrentURL = ClientFunction(() => window.location.href);
         var CurrentURL = await CurrentURL();
         try {
             var productname = await Selector(`${ProductNameElement}`).innerText;
         } catch {
             var productname = "Cant find Product Name"
+            console.log(ProductNameElement)
         }
         try {
             var price = await Selector(`${PriceElement}`).innerText;
         } catch {
             var price = "Cant find Price"
+            console.log(PriceElement)
         }
         try {
             var avalability = await Selector(`${AvalabilityElement}`).innerText;
         } catch {
             var avalability = "Cant find Avalability"
+            console.log(AvalabilityElement)
         }
 
-        fs.readFile('./data.json', 'utf-8', function(err, data) {
+        fs.readFile('./data.json', 'utf-8', function (err, data) {
             if (err) throw err
 
             var arrayOfObjects = JSON.parse(data)
@@ -48,7 +51,7 @@ for (let i = 0; i < 12; i++) {
                 Price: price,
                 Avalability: avalability
             })
-            fs.writeFile('./data.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+            fs.writeFile('./data.json', JSON.stringify(arrayOfObjects), 'utf-8', function (err) {
                 if (err) throw err
                 console.log('Done!')
             })
